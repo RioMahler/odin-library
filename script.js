d356c026439a53af820b17af520a57e726291450
RioMahler <229354130+RioMahler@users.noreply.github.com>
2025-11-09 15:57:12 +0100

    Add books to library array

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const newBookButton = document.getElementById("newBook");
 const dialog = document.querySelector("dialog");
 const submitButton = document.getElementById("submit");
 
+const author = document.getElementById("author");
+const title = document.getElementById("title");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+
 newBookButton.addEventListener("click", () => {
   dialog.showModal();
 });
@@ -18,9 +23,17 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(authorName, titleName, pageCount, readStatus) {
+  titleName = new Book(authorName, titleName, pageCount, readStatus);
+  myLibrary.push(titleName);
+}
 
 function submitData(event) {
-  event.close();
+  let authorName = author.value;
+  let titleName = title.value;
+  let pageCount = pages.value;
+  let readStatus = read.value;
+  addBookToLibrary(authorName, titleName, pageCount, readStatus);
+  dialog.close();
   event.preventDefault();
 }
